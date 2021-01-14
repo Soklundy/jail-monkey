9731dd8f4880f99e3cc1e8193d5d8a63094cc0cc
Sothy Sin (IT) <sothy_sin@phillipbank.com.kh>
2021-01-14 13:46:13 +0700

    JailMonkey remove deprecated method

--- android/src/main/java/com/gantix/JailMonkey/Rooted/RootedCheck.java
+++ android/src/main/java/com/gantix/JailMonkey/Rooted/RootedCheck.java
@@ -26,6 +26,6 @@ public class RootedCheck {
     private static boolean rootBeerCheck(Context context) {
         RootBeer rootBeer = new RootBeer(context);
         
-        return rootBeer.isRootedWithoutBusyBoxCheck();
+        return rootBeer.isRooted();
     }
 }
